84966d885a443206842d8ebf2712db9cbc7d3578
Bruno <b.manchinelli@gmail.com>
2019-03-12 20:48:12 -0300

    Part 2

--- src/com/bmanchi/Main.java
+++ src/com/bmanchi/Main.java
@@ -1,4 +1,12 @@
 package com.bmanchi;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+        long miles = SpeedConverter.toMilesPerHour(10.5);
+        System.out.println("Miles = " + miles);
+
+        SpeedConverter.printConversion(10.5);
+    }
 }
